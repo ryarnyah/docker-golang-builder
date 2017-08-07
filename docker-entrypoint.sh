56d25fc4f914009f4016e1d7789abbb9e42c4e0f
Ryar Nyah <ryarnyah@gmail.com>
2017-08-07 13:50:30 +0200

    Add golang test before build
    Use GOARCH/GOOS only for build

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
-go build -ldflags="-s -w" -o "$(basename $PWD)-${GOOS}-${GOARCH}" && upx --ultra-brute "$(basename $PWD)-${GOOS}-${GOARCH}"
+S_GOOS=${GOOS}
+S_GOARCH=${GOARCH}
+unset GOOS
+unset GOARCH
+
+go test -v && GOARCH=${S_GOARCH} GOOS=${S_GOOS} go build -ldflags="-s -w" -o "$(basename $PWD)-${S_GOOS}-${S_GOARCH}" && upx --ultra-brute "$(basename $PWD)-${S_GOOS}-${S_GOARCH}"
 
